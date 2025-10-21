345fa5677a046e7b131bb28873e6ffb9357632f2
nguyentra04 <nguyentra8704@gmail.com>
2025-10-21 23:08:34 +0700

    day02

---
+++ learning spring core_spring boot day02/learning-spring-core-spring-boot/src/main/java/k23cnt2/nht/day02/learning/spring/core/spring/boot/Application.java
@@ -0,0 +1,13 @@
+package k23cnt2.nht.day02.learning.spring.core.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ learning spring core_spring boot day02/learning-spring-core-spring-boot/src/test/java/k23cnt2/nht/day02/learning/spring/core/spring/boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package k23cnt2.nht.day02.learning.spring.core.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
